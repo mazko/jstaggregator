2c92cc0db376202433f1ddaa93d17abf7d61f280
Oleg Mazko <o.mazko@mail.ru>
2015-10-23 14:26:18 +0300

    ff

--- crossbrowser/Firefox/ff.sh
+++ crossbrowser/Firefox/ff.sh
@@ -4,9 +4,10 @@ rm -rf sdk/data/* && \
 cp -fr ../src/3js sdk/data/ && \
 cp -fr ../src/css sdk/data/ && \
 cp -fr ../src/images sdk/data/ && \
+cp -fr ../../js/Taggregator.js sdk/data/3js && \
 cat sandbox.coffee ../src/urim.coffee | coffee --compile --stdio > sdk/data/urim.js && \
 cd sdk && \
-cfx run --binary-args '-url "http://localhost:8080/" -jsconsole' && \
+jpm run --binary-args '-url "http://localhost:8080/" -jsconsole' && \
 cd -
 
 #--overload-modules
