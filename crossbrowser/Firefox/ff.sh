04546a0a38bf081fd1cf6ebd659e028a6f32751e
Oleg Mazko <o.mazko@mail.ru>
2015-10-23 14:31:16 +0300

    cfx to jpm

--- crossbrowser/Firefox/ff.sh
+++ crossbrowser/Firefox/ff.sh
@@ -7,7 +7,7 @@ cp -fr ../src/images sdk/data/ && \
 cp -fr ../../js/Taggregator.js sdk/data/3js && \
 cat sandbox.coffee ../src/urim.coffee | coffee --compile --stdio > sdk/data/urim.js && \
 cd sdk && \
-jpm run --binary-args '-url "http://localhost:8080/" -jsconsole' && \
+jpm run --binary "`which firefox`" --binary-args 'http://localhost:8080/' && \
 cd -
 
 #--overload-modules
