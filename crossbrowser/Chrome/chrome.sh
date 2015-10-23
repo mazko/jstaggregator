6d6b047c89b02d81d8c5084fa808cb442a31e7da
Oleg Mazko <o.mazko@mail.ru>
2015-10-23 13:20:52 +0300

    crossbrowser -> master

--- crossbrowser/Chrome/chrome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf extention/data/* && \
-cp -fr ../src/3js extention/data/ && \
-cp -fr ../src/css extention/data/ && \
-cp -fr ../src/images extention/data/ && \
-cat sandbox.coffee ../src/urim.coffee | coffee --compile --stdio > extention/data/urim.js
