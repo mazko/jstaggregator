ed5cd3a0977db3aa1a794b127404df05b3ad1f67
Oleg Mazko <o.mazko@mail.ru>
2015-10-26 16:12:21 +0200

    stop up from lucene-5.3.1

--- js/stopaddons/de.js
+++ js/stopaddons/de.js
@@ -2,7 +2,7 @@
  * de stopwords addon for JavaScript tag cloud builder v0.2
  * http://autotagcloud.com/
  *
- * Copyright 2013.03.09, Oleg Mazko
+ * Copyright 2015.10.26, Oleg Mazko
  * http://www.opensource.org/licenses/bsd-license.html
  */
 (function() {
--- stop/fetch.sh
+++ stop/fetch.sh
@@ -1,8 +1,10 @@
+LUCENE_VERSION='5.3.1'
+
 #download Lucene latest, compile
-javac -cp lucene-analyzers-common-4.1.0.jar:lucene-core-4.1.0.jar java/LuceneStopFetcher.java
+javac -cp lucene-analyzers-common-${LUCENE_VERSION}.jar:lucene-core-${LUCENE_VERSION}.jar java/LuceneStopFetcher.java
 
 #start fetching stop words from Lucene
-LuceneStopFetcher="java -cp java:java.LuceneStopFetcher:lucene-analyzers-common-4.1.0.jar:lucene-core-4.1.0.jar LuceneStopFetcher"
+LuceneStopFetcher="java -cp java:java.LuceneStopFetcher:lucene-analyzers-common-${LUCENE_VERSION}.jar:lucene-core-${LUCENE_VERSION}.jar LuceneStopFetcher"
 $LuceneStopFetcher ca CatalanAnalyzer > words/ca
 $LuceneStopFetcher cz CzechAnalyzer > words/cs
 $LuceneStopFetcher da DanishAnalyzer > words/da
@@ -25,4 +27,4 @@ $LuceneStopFetcher sv SwedishAnalyzer > words/sv
 $LuceneStopFetcher tr TurkishAnalyzer > words/tr
 
 #cleanup
-rm java/LuceneStopFetcher.class lucene-analyzers-common-4.1.0.jar lucene-core-4.1.0.jar
+#rm java/LuceneStopFetcher.class lucene-analyzers-common-${LUCENE_VERSION}.jar lucene-core-${LUCENE_VERSION}.jar
